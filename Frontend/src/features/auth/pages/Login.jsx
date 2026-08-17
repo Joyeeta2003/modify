6118da3b19ce63e04f43de93f2d1d2c965cd2a42
Joyeeta Das <joyeetadas597@gmail.com>
2026-08-17 13:21:58 +0530

    login done

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,7 +1,22 @@
-
+import "../style/login.scss"
 const Login = () => {
   return (
-    <div>login</div>
+    <main className="login-page">
+        <div className="form-container">
+            <h1>Login</h1>
+            <form>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" required />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" required />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    </main>
   )
 }
 
