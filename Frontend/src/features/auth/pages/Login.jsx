373f727ce0ee84f30aedd62e50e1a3c21c9727f8
Joyeeta Das <joyeetadas597@gmail.com>
2026-08-19 08:20:51 +0530

    register login Styleing done

--- Frontend/src/features/auth/pages/Login.jsx
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -1,19 +1,15 @@
 import "../style/login.scss"
+import FormGroup from "../components/FormGroup"
+
 const Login = () => {
   return (
     <main className="login-page">
         <div className="form-container">
             <h1>Login</h1>
             <form>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" required />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" name="password" required />
-                </div>
-                <button type="submit">Login</button>
+                <FormGroup label="Email" placeholder="Enter your email"></FormGroup>
+                <FormGroup label="Password" placeholder="Enter your password"></FormGroup>
+                <button type="submit" className="button">Login</button>
             </form>
         </div>
     </main>
